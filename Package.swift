dc59bde54b596c5c6d96e9075c3728cefe54dcbf
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-09-30 16:38:07 -0500

    package updagte

--- Package.swift
+++ Package.swift
@@ -4,20 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "GameLibrary102023",
+    name: "SwiftGameLibrary",
+    platforms: [ iOS(.v16),
+                .macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "GameLibrary102023",
-            targets: ["GameLibrary102023"]),
+            name: "SwiftGameLibrary",
+            targets: ["SwiftGameLibrary"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "GameLibrary102023"),
+            name: "SwiftGameLibrary"),
         .testTarget(
-            name: "GameLibrary102023Tests",
-            dependencies: ["GameLibrary102023"]),
+            name: "SwiftGameLibraryTests",
+            dependencies: ["SwiftGameLibrary"]),
     ]
 )
