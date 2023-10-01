d543d6dd5c2462c9cdebe4e2d4b9f496b8a8bf0f
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-10-01 10:56:15 -0500

    removed parent from protocol
    in this impementation the parent takes care of managing the children

--- Sources/SwiftGameLibrary/GameNode.swift
+++ Sources/SwiftGameLibrary/GameNode.swift
@@ -5,13 +5,13 @@ import Metal
 
 public protocol GameNode: AnyObject, Transformable, Identifiable, Nameable {
     var name: String { get }
-    var parent: (any GameNode)? { get set }
+    //var parent: (any GameNode)? { get set }
     var children: [any GameNode] { get set }
 }
 extension GameNode{
     public func addChild(_ child: any GameNode){
         children.append(child)
-        child.parent = self
+      //  child.parent = self
     }
     public func updateMeAndChildren() {
         if let updateMe = self as? Updateable {
@@ -30,25 +30,4 @@ extension GameNode{
             children[i].updateChildrenMatrices()
         }
     }
-	public func assignModelMatrix(to encoder: MTLRenderCommandEncoder){
-		encoder.setVertexBytes(&modelMatrix, length: simd_float4x4.stride(), index: 1)
-	}
 }
-//public class GameNode: Identifiable{
-//    
-//    public var id: Int = Int.NextID()
-//    public var name: String = ""
-//    public var parent: GameNode?
-//    public var children: [GameNode] = []
-//    
-//    public init(name: String, parent: GameNode? = nil){
-//        self.name = name
-//        self.parent = parent
-//    }
-//    
-//    public func addChild(_ child: GameNode){
-//        children.append(child)
-//        child.parent = self
-//    }
-//    open func doUpdate(deltaTime: Float) {    }
-//}
