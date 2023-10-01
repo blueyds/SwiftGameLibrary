9755fa761d8a903c31fc934a40621c26d5ae191c
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-10-01 10:28:01 -0500

    changed to use new matrixi names

--- Sources/SwiftGameLibrary/Protocols/Transformable.swift
+++ Sources/SwiftGameLibrary/Protocols/Transformable.swift
@@ -8,13 +8,12 @@ public protocol Transformable: Translatable, Rotatable, Scaleable{
 extension Transformable{
     public func calculateModelMatrix(parent: simd_float4x4 = matrix_identity_float4x4)-> simd_float4x4{
         var result = matrix_identity_float4x4
-        result = translateModel(from: result)
-        result = rotateXModel(from: result)
-        result = rotateYModel(from: result)
-        result = rotateZModel(from: result)
-  
-		result = scaleModel(from: result)
-         return matrix_multiply(parent, result)
+        result = matrixTranslateModel(from: result)
+        result = matrixXRotationModel(from: result)
+        result = matrixYRotationModel(from: result)
+        result = matrixZRotationModel(from: result)
+		  result = matrixScaleModel(from: result)
+		  return matrix_multiply(parent, result)
 //		return result
     }
 
