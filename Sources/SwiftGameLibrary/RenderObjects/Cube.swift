150242dd45a0c33fa31f664ced82bdefa8f2db99
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-10-01 10:28:45 -0500

    added cube object

---
+++ Sources/SwiftGameLibrary/RenderObjects/Cube.swift
@@ -0,0 +1,48 @@
+class Cube: CustomMesh{
+    public override func createVertices(r: Float, g: Float, b: Float, a: Float) {
+        vertices = [
+            //Left
+            Vertex(x: -1, y: -1, z: -1, r: r, g: g, b: b, a: a),
+            Vertex(x: -1, y: -1, z: 1, r: r, g: g, b: b, a: a),
+            Vertex(x: -1, y: 1, z: 1, r: r, g: g, b: b, a: a),
+            Vertex(x: -1, y: -1, z: -1, r: r, g: g, b: b, a: a),
+            Vertex(x: -1, y: 1, z: 1, r: r, g: g, b: b, a: a),
+            Vertex(x: -1, y: 1, z: -1, r: r, g: g, b: b, a: a),
+            // Right
+            Vertex(x: 1, y: 1, z: 1, r: r, g: g, b: b, a: a),
+            Vertex(x: 1, y: -1, z: -1, r: r, g: g, b: b, a: a),
+            Vertex(x: 1, y: 1, z: -1, r: r, g: g, b: b, a: a),
+            Vertex(x: 1, y: -1, z: -1, r: r, g: g, b: b, a: a),
+            Vertex(x: 1, y: 1, z: 1, r: r, g: g, b: b, a: a),
+            Vertex(x: 1, y: -1, z: 1, r: r, g: g, b: b, a: a),
+            //top
+            Vertex(x: 1, y: 1, z: 1, r: r, g: g, b: b, a: a),
+            Vertex(x: 1, y: 1, z: -1, r: r, g: g, b: b, a: a),
+            Vertex(x: -1, y: 1, z: -1, r: r, g: g, b: b, a: a),
+            Vertex(x: 1, y: 1, z: 1, r: r, g: g, b: b, a: a),
+            Vertex(x: -1, y: 1, z: -1, r: r, g: g, b: b, a: a),
+            Vertex(x: -1, y: 1, z: 1, r: r, g: g, b: b, a: a),
+            //bottom
+            Vertex(x: 1, y: -1, z: 1, r: r, g: g, b: b, a: a),
+            Vertex(x: -1, y: -1, z: -1, r: r, g: g, b: b, a: a),
+            Vertex(x: 1, y: -1, z: -1, r: r, g: g, b: b, a: a),
+            Vertex(x: 1, y: -1, z: 1, r: r, g: g, b: b, a: a),
+            Vertex(x: -1, y: -1, z: 1, r: r, g: g, b: b, a: a),
+            Vertex(x: -1, y: -1, z: -1, r: r, g: g, b: b, a: a),
+            //back
+            Vertex(x: 1, y: 1, z: -1, r: r, g: g, b: b, a: a),
+            Vertex(x: -1, y: -1, z: -1, r: r, g: g, b: b, a: a),
+            Vertex(x: -1, y: 1, z: -1, r: r, g: g, b: b, a: a),
+            Vertex(x: 1, y: 1, z: -1, r: r, g: g, b: b, a: a),
+            Vertex(x: 1, y: -1, z: -1, r: r, g: g, b: b, a: a),
+            Vertex(x: -1, y: -1, z: -1, r: r, g: g, b: b, a: a),
+            //front
+            Vertex(x: -1, y: 1, z: 1, r: r, g: g, b: b, a: a),
+            Vertex(x: -1, y: -1, z: 1, r: r, g: g, b: b, a: a),
+            Vertex(x: 1, y: -1, z: 1, r: r, g: g, b: b, a: a),
+            Vertex(x: 1, y: 1, z: 1, r: r, g: g, b: b, a: a),
+            Vertex(x: -1, y: 1, z: 1, r: r, g: g, b: b, a: a),
+            Vertex(x: 1, y: -1, z: 1, r: r, g: g, b: b, a: a)
+        ]
+    }
+}
