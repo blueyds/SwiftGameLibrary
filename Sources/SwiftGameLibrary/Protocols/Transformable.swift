28b98f4db58543ffbbc13482fa5a3cc32e87ff04
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-10-01 10:34:41 -0500

    fixed function name
    called wrong name for translation matrix function

--- Sources/SwiftGameLibrary/Protocols/Transformable.swift
+++ Sources/SwiftGameLibrary/Protocols/Transformable.swift
@@ -8,12 +8,12 @@ public protocol Transformable: Translatable, Rotatable, Scaleable{
 extension Transformable{
     public func calculateModelMatrix(parent: simd_float4x4 = matrix_identity_float4x4)-> simd_float4x4{
         var result = matrix_identity_float4x4
-        result = matrixTranslateModel(from: result)
-        result = matrixXRotationModel(from: result)
-        result = matrixYRotationModel(from: result)
-        result = matrixZRotationModel(from: result)
-		  result = matrixScaleModel(from: result)
-		  return matrix_multiply(parent, result)
+        result = matrixTranslationModel(from: result)
+        result = matrixXRotationModel  (from: result)
+        result = matrixYRotationModel  (from: result)
+        result = matrixZRotationModel  (from: result)
+		result = matrixScaleModel      (from: result)
+		return matrix_multiply(parent, result)
 //		return result
     }
 
