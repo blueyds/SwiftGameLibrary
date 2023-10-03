aec0d5b4c8552e1d3003c20d13873780e033c4c1
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-10-03 17:43:02 -0500

    added camera conistants

--- Sources/SwiftGameLibrary/GameConstants.swift
+++ Sources/SwiftGameLibrary/GameConstants.swift
@@ -1,6 +1,8 @@
 public struct BufferIndex{
     public static let Vertex = 0
-    public static let ModelMatrix = 1 
+    public static let ProjectionMatrix = 3
     public static let ViewMatrix = 2
-
+    
+    public static let ModelMatrix = 1 
+    
 }
