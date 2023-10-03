fbf672a7240c206ccbcab15b3bc01dd35f5eea4f
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-10-03 17:41:21 -0500

    added camersa

---
+++ Sources/SwiftGameLibrary/Cameras/BasicCamera.swift
@@ -0,0 +1,31 @@
+public class BasicCamera: Camera{
+    public var name: String
+    public var id: Int = Int.nextID
+    public var fov: Float = 90
+    public var aspectRatio: Float = 1
+    public var near: Float = 1
+    public var far: Float = 1000
+    public var position = simd_float3.Zero
+    public var rotation = simd_float3.Zero
+    public var scale = simd_float3.One
+    public var modelMatrix = matrix_identity_float4x4
+    
+    public init(name: String, fov: Float, aspectRatio: Float, near: Float, far: Float){
+        self.name = name
+        self.fov = fov
+        self.aspectRatio = aspectRatio
+        self.near = near
+        self.far = far
+    }
+    public init(name: String){
+        self.name = name
+    }
+    public render(using encoder: MTLRenderCommandEncoder)-> Bool{
+        modelMatrix = matrixTranslationView(from: matrix_identity_float4x4)
+        encoder.setVertexBytes(&modelMatrix, length: matrix_float4x4.stride(), index: BufferIndex.ViewMatrix)
+        var projection = projectionMatrix()
+        encoder.setVertexBytes(&projection, length: matrix_float4x4.stride(), index: BufferIndex.ProjectionMatrix)
+        return true
+    }
+    
+}
---
+++ Sources/SwiftGameLibrary/Cameras/Camera.swift
@@ -0,0 +1,51 @@
+import SwiftGameLibrary
+import simd
+import Metal
+
+public protocol Camera: GameNode, Renderable{
+    var fov: Float { get set }
+    var aspectRatio: Float { get set }
+    var near: Float { get set }
+    var far: Float { get set }
+    // use render function to assign viewMatrix and 
+    // projectionMatrix 
+}
+
+extension Camera{
+    public var viewMatrix: matrix_float4x4 {
+        modelMatrix
+    }
+    private func assignViewConstants(to encoder: MTLRenderCommandEncoder){
+        
+    }
+    
+    public func drawPrimitives(using: MTLRenderCommandEncoder) { }
+    
+    
+    
+    private func  projectionMatrix() -> matrix_float4x4 {
+        var result = matrix_identity_float4x4
+        let t: Float = tan(fov / 2)
+        result.columns = (
+            simd_float4(1 / (aspectRatio * t),
+                        0,
+                        0,
+                        0),
+            simd_float4(0,
+                        1 / t,
+                        0,
+                        0),
+            simd_float4(0,
+                        0,
+                        -((far + near) / (far - near)),
+                        -1),
+            simd_float4(0,
+                        0,
+                        -((2 * far * near) / (far - near)),
+                        0)
+        )
+        return result
+        
+    }
+}
+
