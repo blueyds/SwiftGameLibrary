a196461ad507ee319fae4025d2280da18681ae22
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-10-04 18:28:29 -0500

    added typealias

--- Sources/SwiftGameLibrary/Extensions/Simd_Float3.swift
+++ Sources/SwiftGameLibrary/Extensions/Simd_Float3.swift
@@ -7,7 +7,9 @@
 
 import simd
 
-extension simd_float3{
-	public static var Zero: simd_float3 { simd_float3(repeating: 0)}
-	public static var One: simd_float3 { simd_float3(repeating: 1)}
+public typealias float3 = simd_float3
+
+extension float3{
+	public static var Zero: float3 { simd_float3(repeating: 0)}
+	public static var One: float3 { simd_float3(repeating: 1)}
 }
