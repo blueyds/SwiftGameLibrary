8630317887c260566c9b8dae0354bdbfa1d932ae
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-10-04 18:01:04 -0500

    error

--- Sources/SwiftGameLibrary/Protocols/Renderable.swift
+++ Sources/SwiftGameLibrary/Protocols/Renderable.swift
@@ -1,6 +1,6 @@
 import Metal
 
-public protocol Renderable: Transformble, Nameable{
+public protocol Renderable: Transformable, Nameable{
     func render(using: MTLRenderCommandEncoder, pipelineChanged: inout Bool)
 	//func drawPrimitives(using: MTLRenderCommandEncoder)
 
