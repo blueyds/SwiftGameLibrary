fc5b53425b58861b90dfabf98477a3ebbed4b518
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-10-04 17:59:31 -0500

    renderable should have a matrix

--- Sources/SwiftGameLibrary/Protocols/Renderable.swift
+++ Sources/SwiftGameLibrary/Protocols/Renderable.swift
@@ -1,6 +1,6 @@
 import Metal
 
-public protocol Renderable: Nameable{
+public protocol Renderable: Transformble, Nameable{
     func render(using: MTLRenderCommandEncoder, pipelineChanged: inout Bool)
 	//func drawPrimitives(using: MTLRenderCommandEncoder)
 
