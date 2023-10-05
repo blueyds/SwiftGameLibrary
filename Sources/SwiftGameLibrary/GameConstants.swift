3354f959d96a419f81271fef797c39f657134b85
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-10-05 08:50:11 -0500

    added more flexibility to grow reserrved buffers

--- Sources/SwiftGameLibrary/GameConstants.swift
+++ Sources/SwiftGameLibrary/GameConstants.swift
@@ -1,6 +1,8 @@
 public struct BufferIndex{
     public static let Vertex = 0
-    public static let ModelMatrix = 1 
-    public static let ViewMatrix = 2
+    // bufers 1 - 10 are reserved for game and scene stuff
+    public static let ViewMatrix = 1
+    // buffers 11 - 20 are reserved for model stuff
+    public static let ModelMatrix = 11 
 
 }
