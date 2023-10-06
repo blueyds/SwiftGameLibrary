31d10560f1100956f77fdc693cf6f39ca3e89e87
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-10-06 09:52:16 -0500

    add initializer for outside projetts

--- Sources/SwiftGameLibrary/Types/Transforms.swift
+++ Sources/SwiftGameLibrary/Types/Transforms.swift
@@ -5,4 +5,10 @@ public struct Transforms{
     public var rotation = float3.Zero
     public var scale = float3.One
     public var matrix = Matrix.identity
+    public init(){}
+    public init(position: float3, rotation: float3, scale: float3){
+        self.position = position
+        self.rotation = rotation
+        self.scale = scale
+    }
 }
