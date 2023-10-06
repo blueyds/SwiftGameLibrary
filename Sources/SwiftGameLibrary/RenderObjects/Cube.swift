b0cc2da3dadb2b6f41692ff44dae229ec5834734
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-10-06 14:29:38 -0500

    added Color

--- Sources/SwiftGameLibrary/RenderObjects/Cube.swift
+++ Sources/SwiftGameLibrary/RenderObjects/Cube.swift
@@ -1,42 +1,70 @@
 public class Cube: CustomMesh{
     public override func createVertices(r: Float, g: Float, b: Float, a: Float) {
-        vertices = [
-            //Left
+        
+    }
+    private func creatLeft(r: Float, g: Float, b: Float, a: Float){
+      //Left
+      let vert = [
             Vertex(x: -1, y: -1, z: -1, r: r, g: g, b: b, a: a),
             Vertex(x: -1, y: -1, z: 1, r: r, g: g, b: b, a: a),
             Vertex(x: -1, y: 1, z: 1, r: r, g: g, b: b, a: a),
             Vertex(x: -1, y: -1, z: -1, r: r, g: g, b: b, a: a),
             Vertex(x: -1, y: 1, z: 1, r: r, g: g, b: b, a: a),
-            Vertex(x: -1, y: 1, z: -1, r: r, g: g, b: b, a: a),
-            // Right
+            Vertex(x: -1, y: 1, z: -1, r: r, g: g, b: b, a: a)
+            ]
+            vertices.append(vert)
+    }
+    private func createRight(r: Float, g: Float, b: Float, a: Float){
+    // Right
+    let vert = [
             Vertex(x: 1, y: 1, z: 1, r: r, g: g, b: b, a: a),
             Vertex(x: 1, y: -1, z: -1, r: r, g: g, b: b, a: a),
             Vertex(x: 1, y: 1, z: -1, r: r, g: g, b: b, a: a),
             Vertex(x: 1, y: -1, z: -1, r: r, g: g, b: b, a: a),
             Vertex(x: 1, y: 1, z: 1, r: r, g: g, b: b, a: a),
-            Vertex(x: 1, y: -1, z: 1, r: r, g: g, b: b, a: a),
-            //top
+            Vertex(x: 1, y: -1, z: 1, r: r, g: g, b: b, a: a)
+            ]
+             vertices.append(vert)
+    }
+    private func createTop(r: Float, g: Float, b: Float, a: Float){
+    //top
+            let vert = [
             Vertex(x: 1, y: 1, z: 1, r: r, g: g, b: b, a: a),
             Vertex(x: 1, y: 1, z: -1, r: r, g: g, b: b, a: a),
             Vertex(x: -1, y: 1, z: -1, r: r, g: g, b: b, a: a),
             Vertex(x: 1, y: 1, z: 1, r: r, g: g, b: b, a: a),
             Vertex(x: -1, y: 1, z: -1, r: r, g: g, b: b, a: a),
-            Vertex(x: -1, y: 1, z: 1, r: r, g: g, b: b, a: a),
-            //bottom
+            Vertex(x: -1, y: 1, z: 1, r: r, g: g, b: b, a: a)
+            ]
+            vertices.append(vert)
+    }
+    private func createBottom(r: Float, g: Float, b: Float, a: Float){
+    //bottom
+    let vert = [
             Vertex(x: 1, y: -1, z: 1, r: r, g: g, b: b, a: a),
             Vertex(x: -1, y: -1, z: -1, r: r, g: g, b: b, a: a),
             Vertex(x: 1, y: -1, z: -1, r: r, g: g, b: b, a: a),
             Vertex(x: 1, y: -1, z: 1, r: r, g: g, b: b, a: a),
             Vertex(x: -1, y: -1, z: 1, r: r, g: g, b: b, a: a),
-            Vertex(x: -1, y: -1, z: -1, r: r, g: g, b: b, a: a),
-            //back
+            Vertex(x: -1, y: -1, z: -1, r: r, g: g, b: b, a: a)
+            ]
+            vertices.append(vert)
+    }
+    private func createBack(r: Float, g: Float, b: Float, a: Float){
+    //back
+    let vert = [
             Vertex(x: 1, y: 1, z: -1, r: r, g: g, b: b, a: a),
             Vertex(x: -1, y: -1, z: -1, r: r, g: g, b: b, a: a),
             Vertex(x: -1, y: 1, z: -1, r: r, g: g, b: b, a: a),
             Vertex(x: 1, y: 1, z: -1, r: r, g: g, b: b, a: a),
             Vertex(x: 1, y: -1, z: -1, r: r, g: g, b: b, a: a),
-            Vertex(x: -1, y: -1, z: -1, r: r, g: g, b: b, a: a),
-            //front
+            Vertex(x: -1, y: -1, z: -1, r: r, g: g, b: b, a: a)
+            ]
+            vertices.append(vert)
+    }
+    private func createFront(r: Float, g: Float, b: Float, a: Float){
+    //front
+        let vert = [
             Vertex(x: -1, y: 1, z: 1, r: r, g: g, b: b, a: a),
             Vertex(x: -1, y: -1, z: 1, r: r, g: g, b: b, a: a),
             Vertex(x: 1, y: -1, z: 1, r: r, g: g, b: b, a: a),
@@ -44,5 +72,16 @@ public class Cube: CustomMesh{
             Vertex(x: -1, y: 1, z: 1, r: r, g: g, b: b, a: a),
             Vertex(x: 1, y: -1, z: 1, r: r, g: g, b: b, a: a)
         ]
+        vertices.append(vert)
+    }
+    public init(position: float4, left: Color, right: Color, top: Color, botton: Color, back: Color, front: Color){
+            self.name = named
+            position(x: x, y: y, z: z)
+            createLeft(r: left.r, g: left.g, b: left.b, a: left.a)
+            createRight(r: right.r, g: right.g, b: right.b, a: right.a)
+            createTop(r: top.r, g: top.g, b: top.b, a: top.a)
+            createBottom(r: bottom.r, g: bottom.g, b: bottom.b, a: botton.a)
+            createBack(r: back.r, g: back.g, b: back.b, a: back.a)
+            createFront(r: front.r, g: front.g, b: front.b, a: front.a)
     }
 }
---
+++ Sources/SwiftGameLibrary/Types/Color.swift
@@ -0,0 +1,31 @@
+import simd
+
+public struct Color{
+    public var r: Float
+    public var g: Float
+    public var b: Float
+    public var a: Float
+    
+    init(_ r: Float, _ g: Float, _ b: Float, _ a:Float ){
+        color
+    }
+    public var vector: float4 {
+        float4(r, g, b, a)
+    }
+    static public func black(a: Float = 1)-> Color{ Color(0,0,0,a)}
+    static public func navy(a: Float = 1)-> Color { Color(0,0,0.5,a)}
+    static public func blue(a: Float = 1)-> Color { Color(0,0,1,a)}
+    static public func green(a: Float = 1)-> Color { Color(0, 0.5, 0, a)}
+    static public func teal(a: Float = 1)-> Color { Color(0, 0.5, 0.5, a)}
+    static public func lime(a: Float = 1)-> Color { Color(0, 1, 0, a)}
+    static public func aqua(a: Float = 1 ) -> Color { Color(0, 1, 1, a)}
+    static public func maroon(a: Float = 1) -> Color { Color(0.5, 0, 0, a)}
+    static public func purple(a: Float = 1) -> Color { Color(0.5, 0, 0.5, a)}
+    static public func olive(a: Float = 1) -> Color { Color(0.5, 0.5, 0, a)}
+    static public func gray(a: Float = 1) -> Color { Color(0.5, 0.5, 0.5, a)}
+    static public func silver(a: Float = 1) -> Color { Color(0.75, 0.75, 0.75, a)}
+    static public func red(a: Float = 1) -> Color { Color(1, 0, 0, a)}
+    static public func fuchsia(a: Float = 1) -> Color { Color(1, 0, 1, a)}
+    static public func yellow(a: Float = 1) -> Color { Color(1, 1, 0, a)}
+    static public func white(a: Float = 1) -> Color { Color(1, 1, 1, a)}
+}
