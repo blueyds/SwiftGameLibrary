d5ab6ba4a3e5f1d05c398701732a68e73a0d16c8
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-10-05 22:17:54 -0500

    added basicscene sclass

---
+++ Sources/SwiftGameLibrary/Scene/BasicScene.swift
@@ -0,0 +1,26 @@
+
+import simd
+import Metal
+
+open class BasicScene: GameScene{
+    public var name: String
+    public var children: [any GameNode] = []
+    public var pipelines: [MTLRenderPipelineState] = []
+    
+
+    
+    public init(named: String, using engine: MainEngine){
+        self.name = named
+        buildPipelines(using: engine)
+        buildScene(using: engine)
+        buildCamera(using: engine)
+    }
+    
+    open func buildPipelines(using: MainEngine){}
+    
+    open func buildScene(using: MainEngine){}
+    
+    open func buildCamera(using: MainEngine) {}
+    
+    
+}
