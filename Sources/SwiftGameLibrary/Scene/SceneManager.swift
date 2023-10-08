54141fe5eb0c52f323a3bab97da63248768f19e4
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-10-08 18:33:44 -0500

    factored render to sepaeate func

--- Sources/SwiftGameLibrary/Scene/SceneManager.swift
+++ Sources/SwiftGameLibrary/Scene/SceneManager.swift
@@ -2,12 +2,18 @@ import MetalKit
 
 public protocol SceneManager{
     func mainLoop(view: MTKView)
+    func render(view: MTKView)
     var currentScene: GameScene? { get set }
     var engine: MainEngine? { get }
+    
 }
 extension SceneManager{
     public func mainLoop(view: MTKView){
         currentScene?.updateScene()
+        render(view: view)
+    }
+    
+    public func render(view: MTKView){
         guard let commandBuffer = engine?.commandQueue?.makeCommandBuffer() else { return }
         guard let passDescriptor = view.currentRenderPassDescriptor else { return }
         guard let encoder = commandBuffer.makeRenderCommandEncoder(descriptor: passDescriptor) else { return }
