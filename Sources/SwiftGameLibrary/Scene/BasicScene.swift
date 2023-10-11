60a18db7514b9ad1f89971a0cfb1b3f8153edcaf
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-10-11 16:45:00 -0500

    moved pipelineState management to SceneManager

--- Sources/SwiftGameLibrary/Scene/BasicScene.swift
+++ Sources/SwiftGameLibrary/Scene/BasicScene.swift
@@ -6,17 +6,14 @@ open class BasicScene: GameScene{
     public var name: String
     public var id: Int = Int.NextID()
     public var children: [any GameNode] = []
-    public var pipelines: [MTLRenderPipelineState] = []
     public var camera: Camera = PerspectiveCamera(named: "Perspective Camera")
     
     public init(named: String, using engine: MainEngine){
         self.name = named
-        buildPipelines(using: engine)
-        buildScene(using: engine)
         buildCamera(using: engine)
-    }
     
-    open func buildPipelines(using: MainEngine){}
+        buildScene(using: engine)
+    }
     
     open func buildScene(using: MainEngine){}
     
