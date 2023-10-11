944e04f6965d9adc5d3f9d5654c03a886c2a7dd1
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-10-11 16:45:35 -0500

    moved pipelineState managment here and added depthstencilState

--- Sources/SwiftGameLibrary/Scene/SceneManager.swift
+++ Sources/SwiftGameLibrary/Scene/SceneManager.swift
@@ -1,24 +1,33 @@
 import MetalKit
 
 public protocol SceneManager{
+    var currentScene: (any GameScene) { get set }
+    var engine: MainEngine { get }
+    var renderPipeline: MTLRenderPipelineState { get }
+    var depthStencilState: MTLDepthStencilState { get } 
+    
     func mainLoop(view: MTKView)
     func render(view: MTKView)
-    var currentScene: (any GameScene)? { get set }
-    var engine: MainEngine? { get }
-    
 }
+
+
 extension SceneManager{
     public func mainLoop(view: MTKView){
-        currentScene?.updateScene()
+        currentScene.updateScene()
         render(view: view)
     }
     
     public func render(view: MTKView){
-        guard let commandBuffer = engine?.commandQueue?.makeCommandBuffer() else { return }
+        guard let commandBuffer = engine.commandQueue?.makeCommandBuffer() else { return }
         guard let passDescriptor = view.currentRenderPassDescriptor else { return }
         guard let encoder = commandBuffer.makeRenderCommandEncoder(descriptor: passDescriptor) else { return }
         if let drawable = view.currentDrawable{
-            currentScene?.renderScene(using: encoder)
+            
+            encoder.setRenderPipelineState(renderPipeline)
+            encoder.setDepthStencilState(depthStencilState)
+            
+            currentScene.renderScene(using: encoder, currentState: renderPipeline)
+            
             encoder.endEncoding()
 			commandBuffer.present(drawable)
 			commandBuffer.commit()
