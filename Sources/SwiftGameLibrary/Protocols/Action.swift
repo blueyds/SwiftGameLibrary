eb45f5764bbdf10a39b8a25b6b744710b74d452b
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-04-09 13:11:12 -0500

    error

--- Sources/SwiftGameLibrary/Protocols/Action.swift
+++ Sources/SwiftGameLibrary/Protocols/Action.swift
@@ -12,7 +12,7 @@ extension Action{
 		}
 	}
 	public func isDead(counter: TickCounter)->Bool{
-		(status == .idle) && (counter - frequency.lastCounter > 60)
+		(status == .idle) && (Int(counter - frequency.lastCounter) > 60)
 	}
 }
 
