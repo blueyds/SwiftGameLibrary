923e6a98775d0328295ba9486bce4a33e5404460
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-04-14 20:38:47 -0500

    removed average

--- Sources/SwiftGameLibrary/Extensions/Array.swift
+++ Sources/SwiftGameLibrary/Extensions/Array.swift
@@ -6,14 +6,3 @@ extension Array {
     }
 }
 
-extension Array where Element == float3{
-    public func sum()->float3{
-        reduce(float3(0, 0, 0), { $0 + $1})
-    }
-   public func average()->Element{
-        let summary = sum()
-        let scale: Float = 1.0 / Float(count)
-        var result = summary * scale
-        return result
-    }
-}
