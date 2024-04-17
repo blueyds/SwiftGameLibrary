7a3c541adc8f479b48ed99aa56af652409c3cf23
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-04-16 22:27:29 -0500

    linted

--- Sources/SwiftGameLibrary/Meshes/Cube.swift
+++ Sources/SwiftGameLibrary/Meshes/Cube.swift
@@ -1,97 +1,89 @@
 public class Cube: VertexCollection{
-    
-    
-    public init(color: Color){
-        super.init(named: "Cube_" + color.name)
-        createVertices(color)
-        finishedBuilding()
+	public init(color: Color){
+		super.init(named: "Cube_" + color.name)
+		createVertices(color)
+		finishedBuilding()
 	}
 	
-    public init(left: Color, right: Color, top: Color, bottom: Color, back: Color, front: Color){
-        super.init(named: "Cube_LEFT" + left.name + 
-                   "_RIGHT" + right.name +
-                   "_TOP" + top.name +
-                   "_BOTTOM" + bottom.name +
-                   "_BACK" + back.name +    
-                   "_FRONT" + front.name)
-        createLeft(left)
-        createRight(right)
-        createTop(top)
-        createBottom(bottom)
-        createBack(back)
-        createFront(front)
-        finishedBuilding()
-    }
-    
-    public  func createVertices(_ color: Color) {
-        
-        createLeft(color)
-        createRight(color)
-        createTop(color)
-        createBottom(color)
-        createBack(color)
-        createFront(color)
-    }
+	public init(left: Color, right: Color, top: Color, bottom: Color, back: Color, front: Color){
+		super.init(named: "Cube_LEFT" + left.name + 
+				"_RIGHT" + right.name +
+				"_TOP" + top.name +
+				"_BOTTOM" + bottom.name +
+				"_BACK" + back.name +    
+				"_FRONT" + front.name)
+		createLeft(left)
+		createRight(right)
+		createTop(top)
+		createBottom(bottom)
+		createBack(back)
+		createFront(front)
+		finishedBuilding()
+	}
 	
-    private func createLeft(_ color: Color){
-        // Left
-        add( -1, -1, -1, color)
-        add( -1, -1,  1, color)
-        add( -1,  1,  1, color)
-        add( -1, -1, -1, color)
-        add( -1,  1,  1, color)
-        add( -1,  1, -1, color)
-        
-    }
-    
-    private func createRight(_ color: Color){
-        // Right
-        add(vertex: Vertex( float3( 1, 1, 1 ), color))
-        add(vertex: Vertex( float3( 1, -1, -1 ), color))
-        add(vertex: Vertex( float3( 1, 1, -1 ), color))
-        add(vertex: Vertex( float3(  1, -1, -1), color))
-        add(vertex: Vertex( float3( 1, 1, 1 ), color))
-        add(vertex: Vertex( float3( 1, -1, 1 ), color))
-    }
-    
-    private func createTop(_ color: Color){
-        //top
-        add(vertex: Vertex( float3( 1, 1, 1  ), color))
-        add(vertex: Vertex( float3( 1, 1, -1), color))
-        add(vertex: Vertex( float3( -1, 1, -1 ), color))
-        add(vertex: Vertex( float3( 1, 1, 1 ), color))
-        add(vertex: Vertex( float3( -1, 1, -1 ), color))
-        add(vertex: Vertex( float3( -1, 1, 1 ), color))
-        
-    }
-    private func createBottom(_ color: Color){
-        //bottom
-        add(vertex: Vertex( float3( 1, -1, 1 ), color))
-        add(vertex: Vertex( float3( -1, -1, -1 ), color))
-        add(vertex: Vertex( float3( 1, -1, -1 ), color))
-        add(vertex: Vertex( float3( 1, -1, 1 ), color))
-        add(vertex: Vertex( float3( -1, -1, 1 ), color))
-        add(vertex: Vertex( float3( -1, -1, -1 ), color))
-    }
-    private func createBack(_ color: Color){
-        //back
-        add(vertex: Vertex( float3( 1, 1, -1 ), color))
-        add(vertex: Vertex( float3( -1, -1, -1 ), color))
-        add(vertex: Vertex( float3( -1, 1, -1 ), color))
-        add(vertex: Vertex( float3( 1, 1, -1 ), color))
-        add(vertex: Vertex( float3(  1, -1, -1), color))
-        add(vertex: Vertex( float3( -1, -1, -1 ), color))
-        
-    }
-    private func createFront(_ color: Color){
-        //front
-        add(vertex: Vertex( float3( -1, 1, 1 ), color))
-        add(vertex: Vertex( float3( -1, -1, 1 ), color))
-        add(vertex: Vertex( float3( 1, -1, 1 ), color))
-        add(vertex: Vertex( float3( 1, 1, 1 ), color))
-        add(vertex: Vertex( float3( -1, 1, 1 ), color))
-        add(vertex: Vertex( float3( 1, -1, 1 ), color))
-        
-    }
-    
+	public  func createVertices(_ color: Color) {
+		createLeft(color)
+		createRight(color)
+		createTop(color)
+		createBottom(color)
+		createBack(color)
+		createFront(color)
+	}
+	
+	private func createLeft(_ color: Color){
+		// Left
+		add( -1, -1, -1, color)
+		add( -1, -1,  1, color)
+		add( -1,  1,  1, color)
+		add( -1, -1, -1, color)
+		add( -1,  1,  1, color)
+		add( -1,  1, -1, color)
+	}
+	
+	private func createRight(_ color: Color){
+		// Right
+		add( 1,  1,  1, color)
+		add( 1, -1, -1, color)
+		add( 1,  1, -1, color)
+		add( 1, -1, -1, color)
+		add( 1,  1,  1, color)
+		add( 1, -1,  1, color)
+	}
+	
+	private func createTop(_ color: Color){
+		//top
+		add(  1, 1,  1, color)
+		add(  1, 1, -1, color)
+		add( -1, 1, -1, color)
+		add(  1, 1,  1, color)
+		add( -1, 1, -1, color)
+		add( -1, 1,  1, color)
+	}
+	private func createBottom(_ color: Color){
+		//bottom
+		add(  1, -1,  1, color)
+		add( -1, -1, -1, color)
+		add(  1, -1, -1, color)
+		add(  1, -1,  1, color)
+		add( -1, -1,  1, color)
+		add( -1, -1, -1, color)
+	}
+	private func createBack(_ color: Color){
+		//back
+		add(  1,  1, -1, color)
+		add( -1, -1, -1, color)
+		add( -1,  1, -1, color)
+		add(  1,  1, -1, color)
+		add(  1, -1, -1, color)
+		add( -1, -1, -1, color)
+	}
+	private func createFront(_ color: Color){
+		//front
+		add( -1,  1, 1, color)
+		add( -1, -1, 1, color)
+		add(  1, -1, 1, color)
+		add(  1,  1, 1, color)
+		add( -1,  1, 1, color)
+		add(  1, -1, 1, color)
+	}
 }
--- Sources/SwiftGameLibrary/Meshes/Mesh.swift
+++ Sources/SwiftGameLibrary/Meshes/Mesh.swift
@@ -1,31 +1,30 @@
 import Metal
 
 public protocol Mesh: AnyObject, Nameable{
-    func render(with: MTLRenderCommandEncoder, _ currentState: MTLRenderPipelineState, at: Transformable)
-    func drawPrimitives(using: MTLRenderCommandEncoder)
+	func render(with: MTLRenderCommandEncoder, _ currentState: MTLRenderPipelineState, at: Transformable)
+	func drawPrimitives(using: MTLRenderCommandEncoder)
 	func assignModel(transforms: Transformable, to: MTLRenderCommandEncoder)
 	func setVertices(to: MTLRenderCommandEncoder)
-    func setPipelineState(on: MTLRenderCommandEncoder, _ didChange: inout Bool)
+	func setPipelineState(on: MTLRenderCommandEncoder, _ didChange: inout Bool)
 }
 
 extension Mesh{
-    public func setVertices(to: MTLRenderCommandEncoder) {}
-    public func setPipelineState(on: MTLRenderCommandEncoder, _ didChange: inout Bool){ didChange = false }
-    public func assignModel(transforms: Transformable, to encoder: MTLRenderCommandEncoder){
+	public func setVertices(to: MTLRenderCommandEncoder) {}
+	public func setPipelineState(on: MTLRenderCommandEncoder, _ didChange: inout Bool){ didChange = false }
+	public func assignModel(transforms: Transformable, to encoder: MTLRenderCommandEncoder){
 		encoder.setVertexBytes(&transforms.modelMatrix, length: Matrix.stride(), index: BufferIndex.ModelMatrix)
 	}
 	
 	public func render(with encoder: MTLRenderCommandEncoder, _ currentState: MTLRenderPipelineState, at transforms: Transformable){
-        encoder.pushDebugGroup(name)
-        var didChange: Bool = false
-        setPipelineState(on: encoder, &didChange)
-        setVertices(to: encoder)
-        assignModel(transforms: transforms, to: encoder)
-        drawPrimitives(using: encoder)
-        if didChange {
-            encoder.setRenderPipelineState(currentState)
-        }
-        encoder.popDebugGroup()
-    }
-
+		encoder.pushDebugGroup(name)
+		var didChange: Bool = false
+		setPipelineState(on: encoder, &didChange)
+		setVertices(to: encoder)
+		assignModel(transforms: transforms, to: encoder)
+		drawPrimitives(using: encoder)
+		if didChange {
+			encoder.setRenderPipelineState(currentState)
+		}
+		encoder.popDebugGroup()
+	}
 }
