8574fe16209589465ee586197f20ef791e7256f0
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-04-23 17:42:01 -0500

    errors

--- Sources/SwiftGameLibrary/Meshes/Sphere.swift
+++ Sources/SwiftGameLibrary/Meshes/Sphere.swift
@@ -11,7 +11,7 @@ public class Sphere: VertexCollection{
 		self.stacks = stacks
 		self.slices = slices
 		createVertices()
-		createBuffer(using: devie)
+		createBuffer(using: device)
 	}
     public func createVertices(){
 		createSphere(.random)
