58f474135d094bbb01ac9d99fdb11b802e854076
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-04-25 21:35:14 -0500

    pass matrix instead of transfromable

--- Sources/SwiftGameLibrary/Meshes/Mesh.swift
+++ Sources/SwiftGameLibrary/Meshes/Mesh.swift
@@ -1,9 +1,9 @@
 import Metal
 
-public protocol Mesh: AnyObject, Nameable{
-	func render(with: MTLRenderCommandEncoder, _ currentState: MTLRenderPipelineState, at: Transformable)
+public protocol Mesh: AnyObject{
+	func render(with: MTLRenderCommandEncoder, _ currentState: MTLRenderPipelineState, modelMatrix: Matrix)
 	func drawPrimitives(using: MTLRenderCommandEncoder)
-	func assignModel(transforms: Transformable, to: MTLRenderCommandEncoder)
+	func assign(modelMatrix: Matrix, to: MTLRenderCommandEncoder)
 	func setVertices(to: MTLRenderCommandEncoder)
 	func setPipelineState(on: MTLRenderCommandEncoder, _ didChange: inout Bool)
 }
@@ -11,20 +11,18 @@ public protocol Mesh: AnyObject, Nameable{
 extension Mesh{
 	public func setVertices(to: MTLRenderCommandEncoder) {}
 	public func setPipelineState(on: MTLRenderCommandEncoder, _ didChange: inout Bool){ didChange = false }
-	public func assignModel(transforms: Transformable, to encoder: MTLRenderCommandEncoder){
-		encoder.setVertexBytes(&transforms.modelMatrix, length: Matrix.stride(), index: BufferIndex.ModelMatrix)
+	public func assign(modelMatrix: Matrix, to encoder: MTLRenderCommandEncoder){
+		encoder.setVertexBytes(&modelMatrix, length: Matrix.stride(), index: BufferIndex.ModelMatrix)
 	}
 	
-	public func render(with encoder: MTLRenderCommandEncoder, _ currentState: MTLRenderPipelineState, at transforms: Transformable){
-		encoder.pushDebugGroup(name)
+	public func render(with encoder: MTLRenderCommandEncoder, _ currentState: MTLRenderPipelineState, modelMatrix: Matrix){
 		var didChange: Bool = false
 		setPipelineState(on: encoder, &didChange)
 		setVertices(to: encoder)
-		assignModel(transforms: transforms, to: encoder)
+		assign(modelMatrix: modelMatrix, to: encoder)
 		drawPrimitives(using: encoder)
 		if didChange {
 			encoder.setRenderPipelineState(currentState)
 		}
-		encoder.popDebugGroup()
 	}
 }
