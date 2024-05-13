06e9065e6ea79be175a7d4357fe0a0a5b0dce0a4
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-05-13 14:32:45 -0500

    error

--- Sources/SwiftGameLibrary/Scene/GameScene.swift
+++ Sources/SwiftGameLibrary/Scene/GameScene.swift
@@ -29,7 +29,7 @@ extension GameScene{
 	
 	public func findAllLights(){
 		lights = []
-		getAllChildren(){child in
+		getAllChildren().forEach(){child in
 			if let light = child as? LightNode{
 				lights.append(light)
 			}
