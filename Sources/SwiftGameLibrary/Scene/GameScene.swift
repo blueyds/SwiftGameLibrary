24cd1824dc3badebba88d0d05093addab7cdbc14
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-05-14 21:21:49 -0500

    error

--- Sources/SwiftGameLibrary/Scene/GameScene.swift
+++ Sources/SwiftGameLibrary/Scene/GameScene.swift
@@ -22,12 +22,12 @@ open class GameScene:Nameable, Identifiable, Actionable, HasChildren{
 	open func doUpdate(counter ticks: TickCounter) { }
 	
 	public func attachLights(to encoder: MTLRenderCommandEncoder){
-		var count: Int16 = lights.count
-		encoder.setFragmentBytes(&count, length: Int16.stride, index: FragmentParameters.LightCount)
+		var count: Int32 = Int32(lights.count)
+		encoder.setFragmentBytes(&count, length: Int32.stride, index: FragmentParameters.LightCount)
 		if count > 0{
 			encoder.setFragmentBytes(&lights[0].position, length: SIMD3<Float>.stride, index: FragmentParameters.LightPosition)
 			encoder.setFragmentBytes(&lights[0].color, length: SIMD3<Float>.stride, index: FragmentParameters.LightColor)
-			encoder.setFragmentBytes(&lights[0].intensity, SIMD4<Float>.stride, index: FragmentParameters.LightIntensity)
+			encoder.setFragmentBytes(&lights[0].intensity, length: SIMD4<Float>.stride, index: FragmentParameters.LightIntensity)
 		}
 	}
 }
