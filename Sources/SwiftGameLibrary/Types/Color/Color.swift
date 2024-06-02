a412051c92a80d8f38f34f73d42785719594133b
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-06-02 09:18:10 -0500

    changed float3/4 to rgb and rgba to better reflect what it is returning and moved

---
+++ Sources/SwiftGameLibrary/Types/Color/Color.swift
@@ -0,0 +1,65 @@
+import simd
+import Metal
+
+public struct Color{
+	public let r: Float
+	public let g: Float
+	public let b: Float
+	public let a: Float
+	
+	public init(_ r: Float, _ g: Float, _ b: Float, _ a:Float){
+		self.r = r
+		self.g = g
+		self.b = b
+		self.a = a
+	}
+	
+	public init(_ r: Float, _ g: Float, _ b: Float){
+		self.r = r
+		self.g = g
+		self.b = b
+		self.a = 1
+	}
+	public init(_ r: Float, _ g: Float, _ b: Float,_ a: Float){
+		self.r = r
+		self.g = g
+		self.b = b
+		self.a = a
+	}
+	
+	public init(_ r: Float, _ g: Float, _ b:Float){
+		self.r = r
+		self.g = g
+		self.b = b
+		self.a = 1
+	}
+	
+	public var rgba: SIMD4<Float> {
+		SIMD4<Float>(r, g, b, a)
+	}
+	public var rgb: SIMD3<Float>{
+		SIMD3<Float>(r, g, b)
+	}
+	
+	public var clearColor: MTLClearColor{
+		MTLClearColorMake(Double(r), Double(g), Double(b), Double(a))
+	}
+	
+
+}
+
+
+extension Color: CustomStringConvertible{
+	public var description: String {
+		"R" + r.description + "_G" + g.description + "_B" + b.description + "_A" + a.description
+	} 
+}
+extension Color{
+	static public var random: Color{
+		let r: Float = .random(in: 0...1)
+		let g: Float = .random(in: 0...1)
+		let b: Float = .random(in: 0...1)
+		return Color(r,g,b,1)
+	}
+	
+}
---
+++ Sources/SwiftGameLibrary/Types/Color/PredefinedColors.swift
@@ -0,0 +1,50 @@
+extension Color{
+	static public var black: Color{ 
+		Color(0,0,0)
+	}
+	static public var navy: Color { 
+		Color(0,0,0.5)
+	}
+	static public var blue: Color { 
+		Color(0,0,1)
+	}
+	static public var green: Color { 
+		Color(0, 0.5, 0)
+	}
+	static public var teal: Color { 
+		Color(0, 0.5, 0.5)
+	}
+	static public var lime: Color { 
+		Color(0, 1, 0)
+	}
+	static public var aqua: Color { 
+		Color(0, 1, 1)
+	}
+	static public var maroon: Color { 
+		Color(0.5, 0, 0)
+	}
+	static public var purple: Color { 
+		Color(0.5, 0, 0.5)
+	}
+	static public var olive: Color { 
+		Color(0.5, 0.5, 0)
+	}
+	static public var gray: Color { 
+		Color(0.5, 0.5, 0.5)
+	}
+	static public var silver: Color { 
+		Color(0.75, 0.75, 0.75)
+	}
+	static public var red: Color { 
+		Color(1, 0, 0)
+	}
+	static public var fuchsia: Color { 
+		Color(1, 0, 1)
+	}
+	static public var yellow: Color { 
+		Color(1, 1, 0)
+	}
+	static public var white: Color { 
+		Color(1, 1, 1)
+	}
+}
