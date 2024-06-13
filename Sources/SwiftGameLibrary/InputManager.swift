ccd73ca67c19f70dd8e9ac651dd6fac12a3123b3
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-06-13 12:43:54 -0500

    added new InputManager class

---
+++ Sources/SwiftGameLibrary/InputManager.swift
@@ -0,0 +1,67 @@
+public class InputManager{
+	public static var shared: InputManager = InputManager()
+	private init(){}
+	
+	private var iButtons: [Int: Bool?] = [:]
+	private var iStrings: [Int:String?] = [:]
+
+	private var sStrings: [String: String?] = [:]
+	
+	private var isLocked: Bool = false 
+	private func waitUnlocked(){
+		while (isLocked) {}
+	}
+	private func lock(){ 
+		waitUnlocked()
+		isLocked = true
+	}
+	private func unlock() { isLocked = false}
+}
+
+// Buttons extensions
+extension InputManager{
+	public func set(button: Int, to value: Bool?){
+		lock()
+		if iButtons.updateValue(value, forKey: button) == nil {
+			print("INFO: InputManager.set(button: \(button)) tried to set a key which was not already set")
+		}
+		unlock()
+	}
+	public func get(button: Int)->Bool?{
+		iButtons[button]
+	}
+	public func initialzeButtons(size: Int){
+		for i in 0..<size{
+			set(button: i, to: false)
+		}
+	}
+}
+
+// Int Strings extensions
+extension InputManager{
+	public func set(stringID: Int, to value: String?){
+		lock()
+		if iStrings.updateValue(value, forKey: stringID) == nil{
+			print("INFO: InputManager.set(stringID: \(stringID) tried to set a key which was not already set")
+		}
+		unlock()
+	}
+	public func get(stringID: Int)->String?{
+		iStrings[stringID]
+	}
+}
+
+// String Strings extensions
+extension InputManager{
+	public func set(property: String, to value: String?){
+		lock()
+		if sStrings.updateValue(value: forKey: property) == nil {
+			print("INFO: InputManager.set(property: \(property) tried to set a key which was not already set")
+		}
+		unlock()
+	}
+	
+	public func get(property: String)->String?{
+		sStrings[property]
+	}
+}
