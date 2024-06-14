bc3d81d76e3fe4ad9016ab4648aa8ac22690c6d1
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-06-13 20:45:12 -0500

    imporoved init

--- Sources/SwiftGameLibrary/GameNodes/LightNode.swift
+++ Sources/SwiftGameLibrary/GameNodes/LightNode.swift
@@ -2,37 +2,41 @@ import simd
 
 public class LightNode: GameNode{
 	public var color: SIMD3<Float> = .one
-	public var intensity: SIMD4<Float> = .one  
+	var intensity: SIMD4<Float> = .one  
 	
 	// public init(named name: String){
 	// 	super.init(named: name)
 	// }
 	
-	public init(color: Color, ambienceIntensity: Float, diffuseIntensity: Float, specularIntensity: Float, brightness: Float){
+	public init(color: Color, ambienceIntensity: Float, diffuseIntensity: Float, specularIntensity: Float, brightness: Float, named name: String? = nil){
 		self.color = color.rgb
-		self.intensity = SIMD4<Float>(brightness, ambienceIntensity, diffuseIntensity, specularIntensity)
-		super.init(prepend: "LIGHT")
+		self.intensity = .zero
+		if name == nil {
+			super.init(prepend: "LIGHT")
+		} else {
+			super.init(name: name)
+		}
+		self.brightness = brightness
+		self.ambienceIntensity = ambienceIntensity
+		self.diffuseIntensity = diffuseIntensity
+		self.specularIntensity = specularIntensity
 	}
-	public init(named name: String, color: Color, ambienceIntensity: Float, diffuseIntensity: Float, specularIntensity: Float, brightness: Float){
-		self.color = color.rgb
-		self.intensity = SIMD4<Float>(brightness, ambienceIntensity, diffuseIntensity, specularIntensity)
-		super.init(named: name)
-	}
+	
 	public var brightness: Float {
 		get{	intensity.x	}
-		set {	intensity.x = newValue	}
+		set{	intensity.x = newValue	}
 	}
 	public var ambienceIntensity: Float {
 		get{	intensity.y	}
-		set {	intensity.y = newValue	}
+		set{	intensity.y = newValue	}
 	}
 	public var diffuseIntensity: Float{
 		get{	intensity.z	}
-		set {	intensity.z = newValue	}
+		set{	intensity.z = newValue	}
 	}
 	public var specularIntensity: Float{
 		get{	intensity.w	}
-		set {	intensity.w = newValue	}
+		set{	intensity.w = newValue	}
 	}
 	
 }
