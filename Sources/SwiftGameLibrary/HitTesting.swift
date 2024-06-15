f69ff044d98470bee538e0d9670be32b41cf9ef3
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-06-14 21:31:11 -0500

    hit testing test

--- Sources/SwiftGameLibrary/HitTesting.swift
+++ Sources/SwiftGameLibrary/HitTesting.swift
@@ -40,17 +40,16 @@ extension GameScene{
 }
 
 extension GameNode{
-    public func hitTest() { }
+    //public func hitTest() { }
     public func isHitTested(ray: Ray, parentScale: SIMD3<Float> = .one)-> HitResult?{
         var result: HitResult? = nil
 		let newScale = SIMD3<Float>(parentScale.x * scale.x, parentScale.y * scale.y, parentScale.z * scale.z)
         let box = AxisAlignedBoundingBox(worldPos: modelMatrix.xyz, lengthOnXAxis: newScale.x, lengthOnYAxis: newScale.y, lengthOnZAxis: newScale.z)
         if let parm = box.intersect(ray){
             result = HitResult(node: self, ray: ray, parameter: parm)
-        }
-		if result == nil {
-			children.forEach{ node in
-				if let hit = node.isHitTested(ray: ray, parentScale: newScale){
+        } else{
+			for key in children.keys {
+				if let hit = children[key].isHitTested(ray: ray, parentScale: newScale){
 					return hit
 				}
 			}
