61f7f5a4b0a0ee353bcaf9913ebc71e933025f60
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-06-14 21:24:10 -0500

    error

--- Sources/SwiftGameLibrary/HitTesting.swift
+++ Sources/SwiftGameLibrary/HitTesting.swift
@@ -43,7 +43,7 @@ extension GameNode{
     public func hitTest() { }
     public func isHitTested(ray: Ray, parentScale: SIMD3<Float> = .one)-> HitResult?{
         var result: HitResult? = nil
-		let newScale = SIMD3<Float(parentScale.x * scale.x, parentScale.y * scale.y, parentScale.z * scale.z)
+		let newScale = SIMD3<Float>(parentScale.x * scale.x, parentScale.y * scale.y, parentScale.z * scale.z)
         let box = AxisAlignedBoundingBox(worldPos: modelMatrix.xyz, lengthOnXAxis: newScale.x, lengthOnYAxis: newScale.y, lengthOnZAxis: newScale.z)
         if let parm = box.intersect(ray){
             result = HitResult(node: self, ray: ray, parameter: parm)
