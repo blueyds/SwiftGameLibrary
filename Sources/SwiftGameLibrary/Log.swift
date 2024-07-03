ac25ac91dc3c61597586068962a573f141c18b03
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-07-03 17:18:54 -0500

    added Log classs to keep messages consistent

---
+++ Sources/SwiftGameLibrary/Log.swift
@@ -0,0 +1,23 @@
+public enum Log{
+	static let separator: String = ":::"
+	static func info(_ msg: String ...){
+		var message = msg.reduce("",reduce(_:_:))
+		print("INFO: \(message)")
+	}
+	
+	static func error(_ msg: String ...){
+		var message = msg.reduce("",reduce(_:_:))
+		print("ERROR: \(message)")
+	}
+	static func fatalError(_ msg: String ...){
+		var message = msg.reduce("",reduce(_:_:))
+		fatalError("\(message)")
+	}
+	
+	static private func reduce(_ initial: String, _ next: String)->String{
+		var result = initial
+		result.append(contentsOf: next)
+		result.append(Log.separator)
+		return result
+	}
+}
