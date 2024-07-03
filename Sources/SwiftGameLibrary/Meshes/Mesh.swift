0604b646dc6aa7494d103b6bbe542f944d0c7ad4
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-07-03 14:36:15 -0500

    added default bounding boxto protocol

--- Sources/SwiftGameLibrary/Meshes/Mesh.swift
+++ Sources/SwiftGameLibrary/Meshes/Mesh.swift
@@ -13,6 +13,11 @@ extension Mesh{
 	
 	public func setPipelineState(on: MTLRenderCommandEncoder, _ didChange: inout Bool){ didChange = false }
 	
+	// default implementation will present a Cube Box that fills a normal 
+	// 2 X 2 X 2 basically from -1 to +1
+	public func getBoundingBox(center: SIMD3<Float>, scaledBy: SIMD3<Float>)->AABB{
+		AxisAlignedBoundingBox(worldPos: center, extantX: scaledBy.x, extantY: scaledBy.y, extantZ: scaledBy.z)
+	}
 	
 	public func render(with encoder: MTLRenderCommandEncoder, _ currentState: MTLRenderPipelineState){
 		var didChange: Bool = false
