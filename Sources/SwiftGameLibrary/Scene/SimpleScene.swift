a186ffabcf98f2b5d42e5ba121317910fa08dd8c
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-07-09 19:38:43 -0500

    converted to a class so we can simply inherit the class for simple scenes

--- Sources/SwiftGameLibrary/Scene/SimpleScene.swift
+++ Sources/SwiftGameLibrary/Scene/SimpleScene.swift
@@ -6,20 +6,27 @@
 //
 
 import Foundation
-public protocol SimpleScene: GameScene{
+public class SimpleScene: GameScene{
+    let id: Int = Int.NextId()
+    var name: String
+    var children: [any GameNode] = []
+    var actions: [any Action] = []
+	var lights: [LightNode] = []
+	var camera: CameraNode!
+    
+    public init(named name: String, using engine: MainEngine){
+        self.name = name
+        buildMeshes(engine: engine)
+        buildScene(engine: engine)
+    }
+    
+	public func buildMeshes(engine: MainEngine) { }
 
-	var lights: [LightNode] { get set }
+	public func buildScene(engine: MainEngine) { }
 
-	var camera: CameraNode! { get set}
-}
+	public func doUpdate(counter ticks: TickCounter) { }
 
-extension SimpleScene{
-	public func buildAll(using engine: MainEngine){
-		buildMeshes(engine: engine)
-		buildScene(engine: engine)
-		camera = findCamera()
-		lights = findAllLights()
-	}
+	public func getLightNodes()->[LightNode] { [] }
 
 	public func getCamera() -> CameraNode {
 		camera
