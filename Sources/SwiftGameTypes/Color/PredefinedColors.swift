6c1bcc8221fc6323e3b795e9c441b3dff993b450
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-08-24 19:33:32 -0500

    missed gamrcolor

--- Sources/SwiftGameTypes/Color/PredefinedColors.swift
+++ Sources/SwiftGameTypes/Color/PredefinedColors.swift
@@ -1,50 +1,50 @@
 extension GameColor{
 	static public var black: GameColor{ 
-		Color(0,0,0)
+		GameColor(0,0,0)
 	}
 	static public var navy: GameColor { 
-		Color(0,0,0.5)
+		GameColor(0,0,0.5)
 	}
 	static public var blue: GameColor { 
-		Color(0,0,1)
+		GameColor(0,0,1)
 	}
 	static public var green: GameColor { 
-		Color(0, 0.5, 0)
+		GameColor(0, 0.5, 0)
 	}
 	static public var teal: GameColor { 
-		Color(0, 0.5, 0.5)
+		GameColor(0, 0.5, 0.5)
 	}
 	static public var lime: GameColor { 
-		Color(0, 1, 0)
+		GameColor(0, 1, 0)
 	}
 	static public var aqua: GameColor { 
-		Color(0, 1, 1)
+		GameColor(0, 1, 1)
 	}
 	static public var maroon: GameColor { 
-		Color(0.5, 0, 0)
+		GameColor(0.5, 0, 0)
 	}
 	static public var purple: GameColor { 
-		Color(0.5, 0, 0.5)
+		GameColor(0.5, 0, 0.5)
 	}
 	static public var olive: GameColor { 
-		Color(0.5, 0.5, 0)
+		GameColor(0.5, 0.5, 0)
 	}
 	static public var gray: GameColor { 
-		Color(0.5, 0.5, 0.5)
+		GameColor(0.5, 0.5, 0.5)
 	}
 	static public var silver: GameColor { 
-		Color(0.75, 0.75, 0.75)
+		GameColor(0.75, 0.75, 0.75)
 	}
 	static public var red: GameColor { 
-		Color(1, 0, 0)
+		GameColor(1, 0, 0)
 	}
 	static public var fuchsia: GameColor { 
-		Color(1, 0, 1)
+		GameColor(1, 0, 1)
 	}
 	static public var yellow: GameColor { 
-		Color(1, 1, 0)
+		GameColor(1, 1, 0)
 	}
 	static public var white: GameColor {
-		Color(1, 1, 1)
+		GameColor(1, 1, 1)
 	}
 }
